2eaf5d37ab23e0277618bf45f04b2c32e8b0d705
IvanFdsh <115582732+IvanFdsh@users.noreply.github.com>
2022-10-24 16:56:06 -0300

    JS M

---
+++ js aula 24-10-22/exe01.js
@@ -0,0 +1,9 @@
+window.alert("Olá, pessoas! Tudo bom com voçês?")
+window.alert("Esta será a segunda mensagem a ser exibida")
+window.confirm("How do you like JavaScript?")
+window.prompt("First Number on your mind")
+window.prompt("Second Number on your mind")
+document.write("I'm typing this for a test, you must ignore...")
+
+Number.parselInt(window.prompt("First Number on your mind"))
+Number.parseFloat(window.prompt("Second Number on your mind"))
